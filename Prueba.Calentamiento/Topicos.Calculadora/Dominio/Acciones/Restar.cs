57aa08692d7a0630c73decef4902127886df6328
Jonart20 <jduran542@gmail.com>
2017-01-28 16:26:59 -0600

    Calculadora

---
+++ Prueba.Calentamiento/Topicos.Calculadora/Dominio/Acciones/Restar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Topicos.Calculadora.Dominio.Acciones
+{
+    public class Restar
+    {
+    }
+}
---
+++ Prueba.Calentamiento/Topicos.Calculadora/Dominio/Acciones/Sumar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Topicos.Calculadora.Dominio.Acciones
+{
+    public class Sumar
+    {
+        int num1 = 0;
+        int num2 = 0;
+    }
+}
